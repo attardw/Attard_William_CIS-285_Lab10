4c0fe4eb7d786b95827dc5599d1852a5d60f02dd
William Attard <attardw@umich.edu>
2022-12-01 08:43:02 -0500

    Made changes in both PosNeg branch and MixDup branch, then committed to master

--- src/Lab10/SelectionSort.java
+++ src/Lab10/SelectionSort.java
@@ -5,21 +5,25 @@ public class SelectionSort {
 	/** Creates a new instance of SelectionSort */
 	    public SelectionSort() {
 	    }
-	    
+
 	    /* A simple SelectionSort algorithm
-	     * pre-condition: 
+	     * pre-condition:
 	     * post-condition:
 	     * inputs:
 	     * outputs:
 	     * special conditions:
-	     */    
+	     */
 	    public int[] basicSelectionSort(int[] x) {
 	        for (int i = 0; i < x.length; ++i) {
+						int min = i;
 	            for (int j= i+1; j < x.length; ++j) {
-	                if (x[i] > x[j]) {
-	                    temp = x[i];
-	                    x[i] = x[j];
-	                    temp = x[j];
+	                if (x[j] < x[i]) {
+	                    min = j;
+											temp = x[min];
+											x[min]=x[i];
+											x[i]=temp;
+											//edit made in mixdup branch because I had already fixed
+											//the sort
 	                }
 	            } // end of inner for loop
 	        } // end of outer for loop
